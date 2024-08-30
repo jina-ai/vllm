405fd1bc346cb54a3766c6e00ec28e1c6a396fa7
numb3r3 <wangfelix87@gmail.com>
2024-08-30 10:42:51 +0800

    fix: format codes

--- vllm/engine/output_processor/stop_checker.py
+++ vllm/engine/output_processor/stop_checker.py
@@ -97,11 +97,14 @@ class StopChecker:
         # Check if the last ngram is repeated in the output text.
         last_token = seq.output_text[-new_char_count:]
         # start checking for repetition after the first 32 tokens
-        if seq.get_output_len() > 32 and self.check_ngram_repetition(seq, sampling_params, last_token):
+        if seq.get_output_len() > 32 and self.check_ngram_repetition(
+                seq, sampling_params, last_token):
             seq.status = SequenceStatus.FINISHED_REPEATED
             return
 
-    def check_ngram_repetition(self, seq: Sequence, sampling_params: SamplingParams, last_token: str) -> bool:
+    def check_ngram_repetition(self, seq: Sequence,
+                               sampling_params: SamplingParams,
+                               last_token: str) -> bool:
         """Check if the last ngram is repeated in the output text.
         """
 
@@ -159,8 +162,6 @@ class StopChecker:
                 is_done = self.repeated_total >= 8
 
         return is_done
-
-
 
     @staticmethod
     def _check_stop_strings(seq: Sequence, new_char_count: int,
