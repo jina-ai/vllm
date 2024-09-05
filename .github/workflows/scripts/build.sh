8f3faa8fa78ad81c285f452d56babf225a2c23f2
numb3r3 <wangfelix87@gmail.com>
2024-09-05 13:55:55 +0800

    fix: release workflow

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -12,7 +12,7 @@ $python_executable -m pip install wheel packaging
 $python_executable -m pip install -r requirements-cuda.txt
 
 # Limit the number of parallel jobs to avoid OOM
-export MAX_JOBS=1
+export MAX_JOBS=4
 # Make sure release wheels are built for the following architectures
 export TORCH_CUDA_ARCH_LIST="7.0 7.5 8.0 8.6 8.9 9.0+PTX"
 # Build
