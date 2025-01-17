8eb9c9055597387c2070eebaa419bae148e82443
numb3r3 <wangfelix87@gmail.com>
2025-01-17 14:28:50 +0800

    fix: draft fix

--- vllm/engine/output_processor/stop_checker.py
+++ vllm/engine/output_processor/stop_checker.py
@@ -1,4 +1,4 @@
-from typing import Callable, List, Optional, Tuple
+from typing import Callable, Optional
 
 from vllm.lora.request import LoRARequest
 from vllm.sampling_params import SamplingParams
@@ -18,16 +18,6 @@ class StopChecker:
         # Do not use it directly, but use `self._get_max_model_len`.
         self._max_model_len = max_model_len
         self.get_tokenizer_for_seq = get_tokenizer_for_seq
-
-        # the position to start checking for repetition
-        self.repeat_start_from = 0
-
-        # the number of tokens repeated
-        self.repeated_count = 0
-        # the gap between the repeated tokens
-        self.repeated_gap = 0
-        # the repeated ngram that we already generated
-        self.repeated_total = 0
 
     def _get_max_model_len(self, lora_req: Optional[LoRARequest]):
         if lora_req and lora_req.long_lora_max_len:
@@ -67,7 +57,7 @@ class StopChecker:
         # Check if a stop token was encountered.
         # This assumes a single token produced per step.
         last_token_id = seq.get_last_token_id()
-        if last_token_id in (sampling_params.stop_token_ids or ()):
+        if last_token_id in sampling_params.stop_token_ids:
             if new_char_count and (
                     not sampling_params.include_stop_str_in_output):
                 # Remove last token
@@ -77,13 +67,9 @@ class StopChecker:
             return
 
         # Check if any stop strings are matched.
-        stop = self.check_stop_strings(
-            seq.output_text, new_char_count, sampling_params.stop,
-            sampling_params.include_stop_str_in_output)
-        if stop is not None:
-            stop_str, truncate_to = stop
-            if truncate_to != -1:
-                seq.output_text = seq.output_text[:truncate_to]
+        stop_str = self._check_stop_strings(seq, new_char_count,
+                                            sampling_params)
+        if stop_str is not None:
             seq.status = SequenceStatus.FINISHED_STOPPED
             seq.stop_reason = stop_str
             return
@@ -101,12 +87,13 @@ class StopChecker:
         # Check if the last ngram is repeated in the output text.
         last_token = seq.output_text[-new_char_count:]
         # start checking for repetition after the first 32 tokens
-        if seq.get_output_len() > 32 and self.check_ngram_repetition(
+        if seq.get_output_len() > 32 and self._check_ngram_repetition(
                 seq, sampling_params, last_token):
             seq.status = SequenceStatus.FINISHED_REPEATED
             return
 
-    def check_ngram_repetition(self, seq: Sequence,
+    @staticmethod
+    def _check_ngram_repetition(seq: Sequence,
                                sampling_params: SamplingParams,
                                last_token: str) -> bool:
         """Check if the last ngram is repeated in the output text.
@@ -120,88 +107,81 @@ class StopChecker:
         repeated_at = None
         repeated_gap = None
 
-        for i, token in enumerate(output_ids[self.repeat_start_from:-1]):
+        for i, token in enumerate(output_ids[seq.repeat_start_from:-1]):
             if token == last_token_id:
-                repeated_at = self.repeat_start_from + i
-                repeated_gap = output_len - repeated_at
+                repeated_at = seq.repeat_start_from + i
+                repeated_gap = output_len - repeated_at - 1
 
         if repeated_at is not None:
-            self.repeated_count += 1
+            seq.repeated_count += 1
             # token_str = self.tokenizer.convert_ids_to_tokens([last_token])[0]
             # print(
             #     f"\n==> token ({last_token}) at {output_len}\n"
             #     f"==> repeat_at: {repeated_at}\n"
-            #     f"==> repeated_count: {self.repeated_count}\n"
+            #     f"==> repeated_count: {seq.repeated_count}\n"
             #     f"==> repeated_gap: {repeated_gap}\n"
-            #     f"==> repeate_start_from: {self.repeat_start_from}"
+            #     f"==> repeate_start_from: {seq.repeat_start_from}"
             # )
 
-            self.repeat_start_from = repeated_at
+            seq.repeat_start_from = repeated_at
 
-        if repeated_at is None or repeated_gap != self.repeated_gap:
-            self.repeated_count = 0
-            self.repeated_gap = 0
-            self.repeated_total = 0
+        if repeated_at is None or repeated_gap != seq.repeated_gap:
+            seq.repeated_count = 0
+            seq.repeated_gap = 0
+            seq.repeated_total = 0
 
         if repeated_gap is not None:
-            self.repeated_gap = repeated_gap
+            seq.repeated_gap = repeated_gap
 
-        if self.repeated_count == self.repeated_gap and self.repeated_gap:
-            self.repeated_total += 1
-            self.repeated_count = 0
+        if seq.repeated_count == seq.repeated_gap and seq.repeated_gap:
+            seq.repeated_total += 1
+            seq.repeated_count = 0
 
-            # print(f"==> repeated_total: {self.repeated_total}")
+            # print(f"==> repeated_total: {seq.repeated_total}")
 
-            repeate_ngram_size = self.repeated_gap
+            repeate_ngram_size = seq.repeated_gap
             # print(f'==> repeate_ngram_size: {repeate_ngram_size}')
 
             if repeate_ngram_size == 1:
                 # single token repetition
-                is_done = self.repeated_total > 64
+                is_done = seq.repeated_total > 64
             elif repeate_ngram_size > 64:
                 # paragraph repetition
-                is_done = self.repeated_total >= 4
+                is_done = seq.repeated_total >= 4
             else:
                 # short ngram repetition?
-                is_done = self.repeated_total >= 8
+                is_done = seq.repeated_total >= 8
 
         return is_done
 
     @staticmethod
-    def check_stop_strings(
-        output_text: str,
-        new_char_count: int,
-        stop: List[str],
-        include_in_output: bool,
-    ) -> Optional[Tuple[str, int]]:
+    def _check_stop_strings(seq: Sequence, new_char_count: int,
+                            sampling_params: SamplingParams) -> Optional[str]:
         """Check if any stop strings are matched and truncate sequence
         output text accordingly.
 
-        Returns tuple (stop_string, offset) if matched or else None.
-
-        Where stop_string is the matched stop string and offset is the
-        length to which output_text should be truncated, or -1 for no
-        truncation.
+        Returns the stop string if matched or else None.
         """
-        if not new_char_count or not stop:
+        if not new_char_count:
             return None
 
-        for stop_str in stop:
+        for stop_str in sampling_params.stop:
             stop_string_len = len(stop_str)
             # Avoid searching already-searched text.
-            stop_index = output_text.find(stop_str,
-                                          -new_char_count - stop_string_len)
+            stop_index = seq.output_text.find(
+                stop_str, -new_char_count - stop_string_len)
             if stop_index == -1:
                 continue
 
-            if include_in_output:
+            if sampling_params.include_stop_str_in_output:
                 # Truncate to end of stop string.
                 stop_index += stop_string_len
-                if stop_index >= len(output_text):
+                if stop_index >= len(seq.output_text):
                     # No truncation required.
-                    return stop_str, -1
+                    return stop_str
 
             # Truncate the output text to either the beginning
             # or end of the stop string.
-            return stop_str, stop_index
+            seq.output_text = seq.output_text[:stop_index]
+            return stop_str
         return None
